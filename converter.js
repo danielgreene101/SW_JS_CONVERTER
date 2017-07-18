5a7ec7c74a0d2b5a515cb01c819bafc2873b6e92
Daniel Greene <d.b.greene101@gmail.com>
2017-07-18 13:20:47 -0500

    finished

--- converter.js
+++ converter.js
@@ -1,24 +1,25 @@
 function toCelsius (tempInput) {
-    return tempInput * 1.8 + 32;
     console.log(tempInput);
+    //return document.getElementById("tempInput").value * 1.8 + 32;
+    document.getElementById("result").innerHTML += document.getElementById("tempInput").value * 1.8 + 32 + '<br>';
 }
 
 function toFahrenheit (temptInput) {
-    return temptInput - 32 * .5556;
     console.log(temptInput);
+    /*return document.getElementById("tempInput").value  - 32 * .5556;*/
+    document.getElementById("result").innerHTML += document.getElementById("tempInput").value  - 32 * .5556 + '<br>';
 }
 
 // Get a reference to the button element in the DOM
-var button = document.getElementById("result");
+var button = document.getElementById("converter");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
-function determineConverter (clickEvent){
-  console.log("event", clickEvent);
-    if (radios[f].checked){
+function determineConverter(){
+    if (document.getElementById("but").checked == true){
         toCelsius();
     }else{
-        (radios[c].checked)
+        document.getElementById("cel").checked == true;
         toFahrenheit();
     }
 }
